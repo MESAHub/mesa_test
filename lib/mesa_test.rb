22aac8658b6493616d64cb01fa3937e1a84bdabb
Bill Wolf <wmwolf@asu.edu>
2019-04-17 12:14:23 -0700

    Prevent failures when choosing rn and re commands for non-SDK builds. Also version bump.

--- lib/mesa_test.rb
+++ lib/mesa_test.rb
@@ -1335,7 +1335,7 @@ class MesaTestCase
     run_start = Time.now
 
     # do the run
-    rn_command = if File.exist?(File.join(ENV['MESASDK_ROOT'], 'bin', 'time'))
+    rn_command = if ENV['MESASDK_ROOT'] && File.exist?(File.join(ENV['MESASDK_ROOT'], 'bin', 'time'))
                    %q(command time -f '%M' -o mem-rn.txt ./rn > out.txt 2> ) +
                      'err.txt'
                  else
@@ -1415,7 +1415,7 @@ class MesaTestCase
 
     # do restart and consolidate output. Command depends on if we have access
     # to SDK version of gnu time.
-    re_command = if File.exist?(File.join(ENV['MESASDK_ROOT'], 'bin', 'time'))
+    re_command = if ENV['MESASDK_ROOT'] && File.exist?(File.join(ENV['MESASDK_ROOT'], 'bin', 'time'))
                    %q(command time -f '%M' -o mem-re.txt ./re ) + "#{photo}" \
                      ' >> out.txt 2> err.txt'
                  else
